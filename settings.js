eed7f87fa170a152a9a39e17dc78bb04dd4da3f7
Sharafsaeedm <120691297+Sharafsaeedm@users.noreply.github.com>
2022-12-22 05:10:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,10 +54,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['9660540637980'] //ur owner number
-global.ownername = "ABDUL RAHMAN🐼🍷 ٫" //ur owner name
+global.vcardowner = ['
+96771613303'] //ur owner number
+global.ownername = "ᯓ᭼|°•𝓱𝒂ۿٰٕہِٰ۬ۛاوۥٰ۬ێ𝔀𝓲•°|❥🇹🇷 ٫" //ur owner name
 global.ytname = "YT: ABDUL RAHMAN" //ur yt chanel name
-global.socialm = "GitHub: ABDUL RAHMAN" //ur github or insta name
+global.socialm = "GitHub: ᯓ᭼|°•𝓱𝒂ۿٰٕہِٰ۬ۛاوۥٰ۬ێ𝔀𝓲•°|❥🇹🇷" //ur github or insta name
 global.location = "Egypt, Cairo, Aizawl" //ur location
 
 //bot bomdy 
